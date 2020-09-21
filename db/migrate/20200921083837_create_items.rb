d3f91088ec17c9c7d2191f8310cafc28d2d95c23
yaman1231 <69840526+yaman1231@users.noreply.github.com>
2020-09-21 18:42:27 +0900

    itemモデル、itemsテーブルの作成

---
+++ db/migrate/20200921083837_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,            null: false, foreign_key: true
+      t.string :name,                null: false, default: ""
+      t.text :description,           null: false
+      t.integer :category_id,        null: false
+      t.integer :condition_id,       null: false
+      t.integer :postage_payer_id,   null: false
+      t.integer :prefecture_id,      null: false
+      t.integer :handling_time_id,   null: false
+      t.integer :price,              null: false
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_092111) do
+ActiveRecord::Schema.define(version: 2020_09_21_083837) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,21 @@ ActiveRecord::Schema.define(version: 2020_09_17_092111) do
     t.integer "genre_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", default: "", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_payer_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "handling_time_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -38,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_09_17_092111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
