4abbed4fd237aeeb81de5d7a7b982c59488f91fd
yaman1231 <69840526+yaman1231@users.noreply.github.com>
2020-09-25 17:07:55 +0900

    削除に失敗した時の処理追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return redirect_to root_path if @item.destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
